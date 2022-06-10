2080c9774daad7011444c8612b7dd0e3e550ae39
TanvirMobasshir <mdtanvirmobasshir@gmail.com>
2022-06-10 16:12:32 +0600

    problem 1 complete

--- code.py
+++ code.py
@@ -1,7 +1,6 @@
-def create_undirected_graph(number_of_edges):
+def create_undirected_graph(number_of_verticies, number_of_edges):
     grph = []
-
-    for m in range(1, number_of_edges + 1):
+    for m in range(number_of_verticies + 1):
         grph.append([])
 
     for i in range(number_of_edges):
@@ -13,41 +12,46 @@ def create_undirected_graph(number_of_edges):
     return grph
 
 
-def bfs(number_of_verticies, grph, src, des):
-    distances = [None]
-    queue = []
-    for j in range(number_of_verticies):
-        distances.append(-1)
+def bfs(grph, src, des):
+    queue = [src]
+
+    distances = [-1] * len(grph)
+    parent = [-1] * len(grph)
 
     distances[src] = 0
-    queue.append(src)
-    print(distances)
-    print()
 
-    while len(queue) != 0:
+    while queue:
         vertex = queue[0]
+        queue.pop(0)
 
         for k in range(len(grph[vertex])):
-            print(f'k: {k}')
-            print(grph[vertex][k])
             if distances[grph[vertex][k]] == -1:
+                parent[grph[vertex][k]] = vertex
                 distances[grph[vertex][k]] = distances[vertex] + 1
-                queue.append(grph[vertex][k])
-            else: print("no action")
-            print(distances)
-            print(f'queue: {queue}')
-            print()
 
-        queue.pop(0)
+                queue.append(grph[vertex][k])
+
+    print(distances[des])
+
+    current = des
+    paths = []
+
+    while parent[current] != -1:
+        step = str(parent[current]) + ' ' + str(current)
+        paths.append(step)
+        current = parent[current]
+
+    s = len(paths) - 1
+    while s >= 0:
+        print(paths[s])
+        s -= 1
 
     return
 
 
 if __name__ == '__main__':
     verticies, edges = map(int, input().split())
-    graph = create_undirected_graph(edges)
+    graph = create_undirected_graph(verticies, edges)
     source, destination = map(int, input().split())
-
-    print(graph)
-
-    bfs(verticies, graph, source, destination)
+    print()
+    bfs(graph, source, destination)
