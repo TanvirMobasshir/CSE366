ec3a43854bf2833296ab90559d988213924fb12e
TanvirMobasshir <mdtanvirmobasshir@gmail.com>
2022-08-22 21:31:37 +0600

    wip\

--- lab_6/code.py
+++ lab_6/code.py
@@ -1,7 +1,12 @@
-def is_safe(graph, v, color, c):
+graph = []
+color = []
+
+
+def is_safe(v, c):
+    global graph, color
     i = 1
-    while len(graph) > i:
-        if graph[v][i] and c == color:
+    while i < len(graph):
+        if graph[v][i] == color[i] and c == color[i]:
             return False
 
         i += 1
@@ -9,16 +14,18 @@ def is_safe(graph, v, color, c):
     return True
 
 
-def graph_coloring(graph, m, color, v):
+def graph_coloring(m, v):
+    global graph, color
+
     if v == len(graph):
         return True
 
     c = 0
     while c < m:
-        if is_safe(graph, v, color, c):
+        if is_safe(v, c):
             color[v] = c
 
-            if graph_coloring(graph, m, color, v + 1):
+            if graph_coloring(m, v + 1):
                 return True
 
             color[v] = -1
@@ -28,37 +35,42 @@ def graph_coloring(graph, m, color, v):
     return False
 
 
-def print_solution(color):
-    i = 0
+def print_solution():
+    global color
+
+    i = 1
     while i < len(color):
         print(color[i])
         i += 1
 
 
-def initialize_graph_coloring(graph, m):
-    color = [-1 for _ in range(len(graph))]
+def initialize_graph_coloring(m):
+    global graph, color
 
-    if not graph_coloring(graph, m, color, 1):
+    color = [-1 for _ in range(len(graph))]
+    if not graph_coloring(m, 1):
         print("NO")
         return
 
-    print_solution(color)
+    print_solution()
 
 
 if __name__ == '__main__':
-    v, e = map(int, input().split())
+    global graph, color
 
+    v, e = map(int, input().split())
     graph = [[0 for _ in range(v + 1)] for _ in range(v + 1)]
+
+    vertex, connectec_vertex = map(int, input().split())
 
     i = 0
     while i < e:
-        vertex, connected_vertex = map(int, input().split())
-        graph[vertex][connected_vertex] = 1
-        graph[connected_vertex][vertex] = 1
+        vertex, connectec_vertex = map(int, input().split())
+        graph[vertex][connectec_vertex] = 1
+        graph[connectec_vertex][vertex] = 1
 
         i += 1
 
     m = int(input())
 
-    print(graph)
-    initialize_graph_coloring(graph, m)
+    initialize_graph_coloring(m)
